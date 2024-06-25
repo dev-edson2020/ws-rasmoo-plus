974ca89274e444e6c4fe43f7a73e08c050145d83
Edson Meira <edson.meira@itixti.com.br>
2024-06-25 14:02:08 -0300

    Testando recurso de usuário

--- src/main/java/com/client/ws/rasmooplus/exception/handler/ResourceHandler.java
+++ src/main/java/com/client/ws/rasmooplus/exception/handler/ResourceHandler.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -48,6 +49,15 @@ public class ResourceHandler {
 
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ErrorResponseDto.builder()
 				.message(Arrays.toString(messages.entrySet().toArray()))
+				.httpStatus(HttpStatus.BAD_REQUEST)
+				.statusCode(HttpStatus.BAD_REQUEST.value())
+				.build());
+	}
+
+	@ExceptionHandler(DataIntegrityViolationException.class)
+	public ResponseEntity<ErrorResponseDto> badRequestException(DataIntegrityViolationException d) {
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ErrorResponseDto.builder()
+				.message(d.getMessage())
 				.httpStatus(HttpStatus.BAD_REQUEST)
 				.statusCode(HttpStatus.BAD_REQUEST.value())
 				.build());
