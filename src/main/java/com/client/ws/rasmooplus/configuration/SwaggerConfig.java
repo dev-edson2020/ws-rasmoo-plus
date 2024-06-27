e929a97e15c6882e0e5fdb414b7f1b9003405959
Edson Meira <edson.meira@itixti.com.br>
2024-06-27 17:22:27 -0300

    Ajustando SwagggerConfig

--- src/main/java/com/client/ws/rasmooplus/configuration/SwaggerConfig.java
+++ src/main/java/com/client/ws/rasmooplus/configuration/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package com.client.ws.rasmooplus.configuration;
 
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -14,34 +15,25 @@ import springfox.documentation.spring.web.plugins.Docket;
 @Configuration
 public class SwaggerConfig {
 
-	@Configuration
-	public class RestTemplateConfig {
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.useDefaultResponseMessages(false)
+				.groupName("v0")
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.client.ws.rasmooplus.controller"))
+				.paths(PathSelectors.any())
+				.build()
+				.apiInfo(apiInfo());
+	}
 
-		//		@Bean
-		//		public RestTemplate restTemplate() {
-		//			return new RestTemplate();
-		//		}
-		//}
+	private ApiInfo apiInfo() {
+		ApiInfoBuilder apiInfoBuilder = new ApiInfoBuilder();
 
-		@Bean
-		public Docket api() {
-			return new Docket(DocumentationType.SWAGGER_2).useDefaultResponseMessages(false)
-					.groupName("v0")
-					.select()
-					.apis(RequestHandlerSelectors.basePackage("com.client.ws.rasmooplus.controller"))
-					.paths(PathSelectors.any())
-					.build()
-					.apiInfo(apiInfo());
-		}
-
-		private ApiInfo apiInfo() {
-			ApiInfoBuilder apiInfoBuilder = new ApiInfoBuilder();
-
-			return apiInfoBuilder.title("Rasmoo Plus")
-					.description("Api para atender o client Rasmoo Plus")
-					.version("0.0.1")
-					.license("Rasmoo cursos de tecnologia")
-					.build();
-		}
+		return apiInfoBuilder.title("Rasmoo Plus")
+				.description("Api para atender o client Rasmoo Plus")
+				.version("0.0.1")
+				.license("Rasmoo cursos de tecnologia")
+				.build();
 	}
 }
