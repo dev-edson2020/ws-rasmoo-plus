adcbbed8a3a2be712bc8d825900a72a53c92251e
Edson Meira <edson.meira@itixti.com.br>
2024-06-28 21:26:29 -0300

    Ajustando SwagggerConfig

--- src/main/java/com/client/ws/rasmooplus/configuration/SwaggerConfig.java
+++ src/main/java/com/client/ws/rasmooplus/configuration/SwaggerConfig.java
@@ -2,30 +2,28 @@ package com.client.ws.rasmooplus.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.web.client.RestTemplate;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.info.Contact;
 
 @Configuration
 public class SwaggerConfig {
 
-//	@Bean
-//	public Docket api() {
-//		return new Docket(DocumentationType.SWAGGER_2)
-//				.useDefaultResponseMessages(false)
-//				.groupName("v0")
-//				.select()
-//				.apis(RequestHandlerSelectors.basePackage("com.client.ws.rasmooplus.controller"))
-//				.paths(PathSelectors.any())
-//				.build()
-//				.apiInfo(apiInfo());
-//	}
-//
-//	private ApiInfo apiInfo() {
-//		ApiInfoBuilder apiInfoBuilder = new ApiInfoBuilder();
-//
-//		return apiInfoBuilder.title("Rasmoo Plus")
-//				.description("Api para atender o client Rasmoo Plus")
-//				.version("0.0.1")
-//				.license("Rasmoo cursos de tecnologia")
-//				.build();
-//	}
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+	@Bean
+	public OpenAPI customOpenAPI() {
+		return new OpenAPI()
+				.info(new Info()
+						.title("Rasmoo Plus")
+						.description("API para atender o cliente Rasmoo Plus")
+						.version("0.0.1")
+						.license(new License().name("Rasmoo cursos de tecnologia"))
+						.contact(new Contact().name("Rasmoo").url("https://www.rasmoo.com").email("contact@rasmoo.com")));
+	}
 }
