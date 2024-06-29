c9bef0fbc07cd5d0c37f7503d4f81807388378ac
Edson Meira <edson.meira@itixti.com.br>
2024-06-29 14:50:58 -0300

    Iniciando test na classe WsRaspayIntegrationImpl

--- src/main/java/com/client/ws/rasmooplus/integration/impl/WsRaspayIntegrationImpl.java
+++ src/main/java/com/client/ws/rasmooplus/integration/impl/WsRaspayIntegrationImpl.java
@@ -1,50 +1,65 @@
 package com.client.ws.rasmooplus.integration.impl;
 
+import com.client.ws.rasmooplus.dto.wsraspay.CustomerDto;
+import com.client.ws.rasmooplus.dto.wsraspay.OrderDto;
+import com.client.ws.rasmooplus.dto.wsraspay.PaymentDto;
+import com.client.ws.rasmooplus.exception.HttpClientException;
+import com.client.ws.rasmooplus.integration.WsRaspayIntegration;
+import org.apache.tomcat.util.codec.binary.Base64;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
-import com.client.ws.rasmooplus.dto.wsraspay.CustomerDto;
-import com.client.ws.rasmooplus.dto.wsraspay.OrderDto;
-import com.client.ws.rasmooplus.dto.wsraspay.PaymentDto;
-import com.client.ws.rasmooplus.integration.WsRaspayIntegration;
-
 @Component
 public class WsRaspayIntegrationImpl implements WsRaspayIntegration {
 
 	private final RestTemplate restTemplate;
+	private final HttpHeaders headers;
 
-	public WsRaspayIntegrationImpl(RestTemplate restTemplate) {
-		this.restTemplate = restTemplate;
+	public WsRaspayIntegrationImpl(){
+		restTemplate = new RestTemplate();
+		headers = getHttpHeaders();
 	}
 
 	@Override
 	public CustomerDto createCustomer(CustomerDto dto) {
 		try {
-			HttpEntity<CustomerDto> request = new HttpEntity<>(dto);
-			ResponseEntity<CustomerDto> response = restTemplate.exchange(
-					"http://localhost:8081/ws-raspay/v1/customer",
-					HttpMethod.POST,
-					request,
-					CustomerDto.class
-			);
+			HttpEntity<CustomerDto> request = new HttpEntity<>(dto,this.headers);
+			ResponseEntity<CustomerDto> response =
+					restTemplate.exchange("http://localhost:8081/ws-raspay/v1/customer", HttpMethod.POST, request, CustomerDto.class);
 			return response.getBody();
 		} catch (Exception e) {
-			throw new RuntimeException("Failed to create customer", e);
+			throw e;
 		}
 	}
 
 	@Override
 	public OrderDto createOrder(OrderDto dto) {
-		// Implement the logic for creating an order here
-		return null;
+		try {
+			HttpEntity<OrderDto> request = new HttpEntity<>(dto,this.headers);
+			ResponseEntity<OrderDto> response =
+					restTemplate.exchange("http://localhost:8081/ws-raspay/v1/order", HttpMethod.POST, request, OrderDto.class);
+			return response.getBody();
+		} catch (Exception e) {
+			throw e;
+		}
 	}
 
 	@Override
 	public Boolean processPayment(PaymentDto dto) {
-		// Implement the logic for processing a payment here
 		return null;
 	}
+
+	private HttpHeaders getHttpHeaders() {
+		HttpHeaders headers = new HttpHeaders();
+		String credential = "rasmooplus:r@sm00";
+		String base64 = new String(Base64.encodeBase64(credential.getBytes(), false));
+		headers.add("Authorization", "Basic " + base64);
+		return headers;
+	}
+
 }
--- src/test/java/com/client/ws/rasmooplus/integration/WsRaspayIntegrationImplTest.java
+++ src/test/java/com/client/ws/rasmooplus/integration/WsRaspayIntegrationImplTest.java
@@ -1,95 +1,21 @@
 package com.client.ws.rasmooplus.integration;
 
-import com.client.ws.rasmooplus.dto.wsraspay.CustomerDto;
-import com.client.ws.rasmooplus.exception.HttpClientException;
-import com.client.ws.rasmooplus.integration.impl.WsRaspayIntegrationImpl;
-import org.apache.tomcat.util.codec.binary.Base64;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.reactivestreams.Publisher;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.test.util.ReflectionTestUtils;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.Optional;
-import java.util.UUID;
+import com.client.ws.rasmooplus.dto.wsraspay.CustomerDto;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.when;
+@SpringBootTest
+ class WsRaspayIntegrationImplTest {
 
-
-@ExtendWith(MockitoExtension.class)
-class WsRaspayIntegrationTest {
-
-	@Mock
-	private RestTemplate restTemplate;
-
-	@InjectMocks
-	private WsRaspayIntegrationImpl wsRaspayIntegration;
-
-	private static HttpHeaders headers;
-
-	@BeforeAll
-	public static void loadHeaders() {
-		headers = getHttpHeaders();
-	}
+	@Autowired
+	private WsRaspayIntegration wsRaspayIntegration;
 
 	@Test
-	void given_createCustomer_when_apiResponseIs201Created_then_returnCustomerDto() {
-		ReflectionTestUtils.setField(wsRaspayIntegration,"raspayHost","http://localhost:8080");
-		ReflectionTestUtils.setField(wsRaspayIntegration,"customerUrl","/customer");
-		CustomerDto dto = new CustomerDto();
-		dto.setCpf("11111111111");
-		HttpEntity<CustomerDto> request = new HttpEntity<>(dto,this.headers);
-		when(restTemplate.exchange("http://localhost:8080/customer", HttpMethod.POST, request, CustomerDto.class))
-				.thenReturn(ResponseEntity.of(Optional.of(dto)));
-		assertEquals(dto, wsRaspayIntegration.createCustomer(dto));
-		Mockito.verify(restTemplate, times(1)).exchange("http://localhost:8080/customer", HttpMethod.POST, request, CustomerDto.class);
-	}
-
-	@Test
-	void given_createCustomer_when_apiResponseIs400BadRequest_then_returnNull() {
-		ReflectionTestUtils.setField(wsRaspayIntegration,"raspayHost","http://localhost:8080");
-		ReflectionTestUtils.setField(wsRaspayIntegration,"customerUrl","/customer");
-		CustomerDto dto = new CustomerDto();
-		dto.setCpf("11111111111");
-		HttpEntity<CustomerDto> request = new HttpEntity<>(dto,this.headers);
-		when(restTemplate.exchange("http://localhost:8080/customer", HttpMethod.POST, request, CustomerDto.class))
-				.thenReturn(ResponseEntity.badRequest().build());
-		assertNull(wsRaspayIntegration.createCustomer(dto));
-		Mockito.verify(restTemplate, times(1)).exchange("http://localhost:8080/customer", HttpMethod.POST, request, CustomerDto.class);
-	}
-
-//	@Test
-//	void given_createCustomer_when_apiResponseGetThrows_then_throwHttpClientException() {
-//		ReflectionTestUtils.setField(wsRaspayIntegration,"raspayHost","http://localhost:8080");
-//		ReflectionTestUtils.setField(wsRaspayIntegration,"customerUrl","/customer");
-//		CustomerDto dto = new CustomerDto();
-//		dto.setCpf("11111111111");
-//		HttpEntity<CustomerDto> request = new HttpEntity<>(dto,this.headers);
-//		when(restTemplate.exchange("http://localhost:8080/customer", HttpMethod.POST, request, CustomerDto.class))
-//				.thenThrow(HttpClientException.class);
-//
-//		assertThrows(HttpClientException.class, () -> wsRaspayIntegration.createCustomer(dto));
-//		Mockito.verify(restTemplate, times(1)).exchange("http://localhost:8080/customer", HttpMethod.POST, request, CustomerDto.class);
-//	}
-
-	private static HttpHeaders getHttpHeaders() {
-		HttpHeaders headers = new HttpHeaders();
-		String credential = "rasmooplus:r@sm00";
-		String base64 = new String (Base64.encodeBase64(credential.getBytes()));
-		headers.add("Authorization","Basic "+base64);
-		return headers;
+	void createCustomerWhenDtoOK() {
+		CustomerDto dto = new CustomerDto(null, "27079871824", "edsonanga@hotmail.com", "Edson", "Meira");
+		wsRaspayIntegration.createCustomer(dto);
 	}
 
 }
