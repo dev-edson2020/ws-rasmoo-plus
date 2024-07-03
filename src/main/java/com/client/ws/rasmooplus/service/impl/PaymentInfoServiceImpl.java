75e7998189116f3d26bed44e7ac20b579061b54d
Edson Meira <edson.meira@itixti.com.br>
2024-07-02 21:02:22 -0300

    Executando o processo de pagamento

--- src/main/java/com/client/ws/rasmooplus/service/impl/PaymentInfoServiceImpl.java
+++ src/main/java/com/client/ws/rasmooplus/service/impl/PaymentInfoServiceImpl.java
@@ -5,10 +5,17 @@ import java.util.Objects;
 import org.springframework.stereotype.Service;
 
 import com.client.ws.rasmooplus.dto.PaymentProcessDto;
-import com.client.ws.rasmooplus.dto.UserPaymentInfoDto;
+import com.client.ws.rasmooplus.dto.wsraspay.CustomerDto;
+import com.client.ws.rasmooplus.dto.wsraspay.OrderDto;
+import com.client.ws.rasmooplus.dto.wsraspay.PaymentDto;
 import com.client.ws.rasmooplus.exception.NotFoundException;
 import com.client.ws.rasmooplus.exception.handler.BusinessException;
+import com.client.ws.rasmooplus.integration.WsRaspayIntegration;
 import com.client.ws.rasmooplus.mapper.UserPaymentInfoMapper;
+import com.client.ws.rasmooplus.mapper.wsraspay.CreditCardMapper;
+import com.client.ws.rasmooplus.mapper.wsraspay.CustomerMapper;
+import com.client.ws.rasmooplus.mapper.wsraspay.OrderMapper;
+import com.client.ws.rasmooplus.mapper.wsraspay.PaymentMapper;
 import com.client.ws.rasmooplus.model.User;
 import com.client.ws.rasmooplus.model.UserPaymentInfo;
 import com.client.ws.rasmooplus.repository.UserPaymentInfoRepository;
@@ -20,31 +27,44 @@ public class PaymentInfoServiceImpl implements PaymentInfoService {
 
 	private final UserRepository userRepository;
 	private final UserPaymentInfoRepository userPaymentInfoRepository;
+	private final WsRaspayIntegration wsRaspayIntegration;
 
-	PaymentInfoServiceImpl(UserRepository userRepository, UserPaymentInfoRepository userPaymentInfoRepository) {
+	PaymentInfoServiceImpl(UserRepository userRepository, UserPaymentInfoRepository userPaymentInfoRepository,
+			WsRaspayIntegration wsRaspayIntegration) {
 		this.userRepository = userRepository;
 		this.userPaymentInfoRepository = userPaymentInfoRepository;
+		this.wsRaspayIntegration = wsRaspayIntegration;
 	}
 
 	@Override
-		public Boolean process(PaymentProcessDto dto) {
-			//Verifica usuário por Id e verifica se existe assinatura
-			var userOpt = userRepository.findById(dto.getUserPaymentInfoDto().getUserId());
-			if(userOpt.isEmpty()) {
-				throw new NotFoundException("Usuário não encontrado");
-			}
-			User user = userOpt.get();
-			if(Objects.nonNull(user.getSubscriptionType())) {
-				throw new BusinessException("Pagamento não pode ser processado porque o usuário já possui assinatura");
-			}
+	public Boolean process(PaymentProcessDto dto) {
+		//Verifica usuário por Id e verifica se existe assinatura
+		var userOpt = userRepository.findById(dto.getUserPaymentInfoDto()
+				.getUserId());
+		if(userOpt.isEmpty()) {
+			throw new NotFoundException("Usuário não encontrado");
+		}
+		User user = userOpt.get();
+		if(Objects.nonNull(user.getSubscriptionType())) {
+			throw new BusinessException("Pagamento não pode ser processado porque o usuário já possui assinatura");
+		}
 
 		//Cria ou atualiza usuári no Raspay
+		CustomerDto customerDto = wsRaspayIntegration.createCustomer(CustomerMapper.build(user));
 		//Cria o pedido de pagamento
+		OrderDto orderDto = wsRaspayIntegration.createOrder(OrderMapper.build(customerDto.getId(), dto));
 		//Processa o pagamento
+		PaymentDto paymentDto = PaymentMapper.build(customerDto.getId(), orderDto.getId(),
+				CreditCardMapper.build(dto.getUserPaymentInfoDto(), user.getCpf()));
+		Boolean successPayment = wsRaspayIntegration.processPayment(paymentDto);
+		if(successPayment) {
+			//Salvar informações de pagamento
+			UserPaymentInfo userPaymentInfo = UserPaymentInfoMapper.fromDtoToEntity(dto.getUserPaymentInfoDto(), user);
+			userPaymentInfoRepository.save(userPaymentInfo);
+		}
 
-		//Salvar informações de pagamento
-		UserPaymentInfo userPaymentInfo = UserPaymentInfoMapper.fromDtoToEntity(dto.getUserPaymentInfoDto(), user);
-		userPaymentInfoRepository.save(userPaymentInfo);
+
+
 
 		//Enviar email de confirmação de conta
 		//Retorna sucesso ou não do pagamento
