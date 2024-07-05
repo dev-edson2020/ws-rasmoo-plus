59a1f1b02bd5fd08722f4eb1c3fd6fb346eb3e8a
Edson Meira <edson.meira@itixti.com.br>
2024-07-05 13:10:10 -0300

    Corrigindo Implementação JWT

--- src/main/java/com/client/ws/rasmooplus/service/AuthenticationService.java
+++ src/main/java/com/client/ws/rasmooplus/service/AuthenticationService.java
@@ -1,5 +1,7 @@
 package com.client.ws.rasmooplus.service;
 
+import org.springframework.context.annotation.Bean;
+
 import com.client.ws.rasmooplus.dto.LoginDto;
 import com.client.ws.rasmooplus.dto.TokenDto;
 
